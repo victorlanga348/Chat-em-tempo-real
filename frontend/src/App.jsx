fbbb8f2bad71c2b1c09d90759e6c4266db636db6
Victor Langa <victorlanga720@gmail.com>
2026-04-19 17:27:57 +0200

    Estrutura inicial do DevChat: MVC, Prisma v6 e React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <h1 className="text-3xl font-bold underline">Chat em Tempo Real</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
