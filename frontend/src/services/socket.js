f9a50ed46cfad511384735930bda0b545bc4a8e5
Victor Langa <victorlanga720@gmail.com>
2026-04-30 13:55:08 +0200

    voltando os links do banco da render

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Endereço do seu Backend
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = 'http://localhost:3000' || 'https://backend-do-chat.onrender.com';
 
 // Conectamos ao servidor
 export const socket = io(SOCKET_URL, {
